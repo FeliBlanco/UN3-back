0a4bf4a1d2728277203e2fc63791745e1ca153ec
FeliBlanco <felipeblanco09@hotmail.com>
2024-10-22 23:17:06 -0300

    a ver ahora

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-    await mongoose.connect('mongodb://feli:feliblancoo@ec2-18-206-120-192.compute-1.amazonaws.com:27017/piii?authSource=admin')
+    await mongoose.connect('mongodb://feli:feliblanco@ec2-52-90-17-128.compute-1.amazonaws.com:27017/piii')
     console.log(`DB CONECTADA!`)
 }
 
